78204fbda75e55af449ec26cf7096481543c7164
Silvia Parada <silviaparadag@yahoo.es>
2023-09-05 16:41:55 +0200

    Get Data from Api

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,38 @@
 'use strict';
 /* */
+
 console.log('>> Ready :)');
+
+const productItem = document.querySelector('.js-products');
+
+let productsList = [];
+
+const getApiData = () => {
+  fetch('../api/data.json')
+    .then((response) => response.json())
+    .then((data) => {
+      productsList = data.cart.items;
+      renderProductList();
+    });
+};
+
+const getProductHtml = (product) => {
+  let html = '';
+  html += `<article class="card">`;
+  html += `  <img src="${product.imageUrl}" class="card__img" alt="Product: ${product.name}">`;
+  html += `  <h3 class="card__title">${product.name}</h3>`;
+  html += `  <p class="card__description">${product.price} €</p>`;
+  html += `  <button class="js-add-product card__btn" data-id="${product.id}">Add to cart</button>`;
+  html += `</article>`;
+  return html;
+};
+
+const renderProductList = () => {
+  let productsCode = '';
+  for (const product of productsList) {
+    productsCode += getProductHtml(product);
+  }
+  productItem.innerHTML = productsCode;
+};
+
+getApiData();

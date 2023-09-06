29a127de3aab8c04da73074e3b9ce6d11036a01d
Silvia Parada <silviaparadag@yahoo.es>
2023-09-06 11:30:08 +0200

    Change section of ul elem

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
 console.log('>> Ready :)');
 
 const productItemList = document.querySelector('.js-products');
+const addBtn = document.querySelector('.js-add-product');
 
 let productsList = [];
 
@@ -18,12 +19,12 @@ const getApiData = () => {
 
 const renderOneProduct = (product) => {
   let html = '';
-  html += `<article class="card">`;
+  html += `<li class="js_card "><article class="card">`;
   html += `  <img src="${product.imageUrl}" class="card__img" alt="Product: ${product.name}">`;
   html += `  <h3 class="card__title">${product.name}</h3>`;
   html += `  <p class="card__description">${product.price} €</p>`;
   html += `  <button class="js-add-product card__btn" data-id="${product.id}">Add to cart</button>`;
-  html += `</article>`;
+  html += `</article></li>`;
   return html;
 };
 
@@ -33,7 +34,6 @@ const renderProductList = () => {
     productItemList.innerHTML += renderOneProduct(eachProduct);
   }
 };
-/*
- */
 
 getApiData();
+/* */
